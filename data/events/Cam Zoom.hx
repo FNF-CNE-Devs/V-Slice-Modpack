fa2613c74ee3b331af610d810bb64797ee084899
drfiffi <drfiffi123@gmail.com>
2024-09-20 07:05:11 -0400

    Just some optimizations

--- data/events/Cam Zoom.hx
+++ data/events/Cam Zoom.hx
@@ -3,8 +3,12 @@ function onEvent(_) {
         if (_.event.params[1] == null || _.event.params[2] == null){
             defaultCamZoom = _.event.params[0];
         } else {
-            FlxTween.cancelTweensOf(FlxG.camera, ['zoom']);
-            FlxTween.tween(FlxG.camera, {zoom: _.event.params[0]}, (Conductor.stepCrochet / 1000) * _.event.params[1], {ease: Reflect.field(FlxEase, _.event.params[2]), onComplete: function(value) {defaultCamZoom = FlxG.camera.zoom;}});
+            zoomTween.cancel();
+            zoomTween = FlxTween.tween(FlxG.camera, {zoom: _.event.params[0]}, (Conductor.stepCrochet / 1000) * _.event.params[1], {ease: Reflect.field(FlxEase, _.event.params[2]), onComplete: function(value) {defaultCamZoom = FlxG.camera.zoom;}});
         }
     }
+}
+
+function create(){
+    zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1);
 }
