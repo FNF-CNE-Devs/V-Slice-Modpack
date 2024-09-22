46ae65906370c9915132f54838520c6caaefeb9f
drfiffi <drfiffi123@gmail.com>
2024-09-22 03:10:22 -0400

    Light Cleaning
    nyehehe

--- data/events/Cam Zoom.hx
+++ data/events/Cam Zoom.hx
@@ -1,14 +1,18 @@
+stopFakeZoom = true;
+
 function onEvent(_) {
     if (_.event.name == 'Cam Zoom') {
         if (_.event.params[1] == null || _.event.params[2] == null){
             defaultCamZoom = _.event.params[0];
         } else {
             zoomTween.cancel();
-            zoomTween = FlxTween.tween(FlxG.camera, {zoom: _.event.params[0]}, (Conductor.stepCrochet / 1000) * _.event.params[1], {ease: Reflect.field(FlxEase, _.event.params[2]), onComplete: function(value) {defaultCamZoom = FlxG.camera.zoom;}});
+            stopFakeZoom = false;
+            zoomTween = FlxTween.tween(FlxG.camera, {zoom: _.event.params[0]}, (Conductor.stepCrochet / 1000) * _.event.params[1], {ease: Reflect.field(FlxEase, _.event.params[2]), onComplete: function(value) {
+                defaultCamZoom = FlxG.camera.zoom;
+                stopFakeZoom = true;
+            }});
         }
     }
 }
 
-function create(){
-    zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1);
-}
+function create() zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1);
--- data/states/StoryMenuState.hx
+++
@@ -1,3 +0,0 @@
-//
-function create()
-	scoreMessage = "LEVEL SCORE:{0}"
