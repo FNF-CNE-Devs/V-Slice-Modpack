ee7edfa1814684465d9c554375079f50f518e208
drfiffi <drfiffi123@gmail.com>
2024-09-26 08:58:13 -0400

    Week 6 Erect Update & Bugfix
    Fully ported Week 6 Erect songs' charts, events and metadata ALONG WITH fixing a bug with ZoomCamera from previous update where it used the previously used "event.event.name" name.
    
    Next up will be Week 1 redone :3

--- data/events/ZoomCamera.hx
+++ data/events/ZoomCamera.hx
@@ -1,9 +1,7 @@
-var tweenedZoomPLEASEWORK:Bool = false;
 function create() zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1); // just used so zoomTween.cancel() can work
 function onEvent(_) {
-    if (_.event.name == 'Cam Zoom') {
-        if (_.event.params[1] == null && _.event.params[2] == null){
-            defaultCamZoom = _.event.params[0];
+    if (_.event.name == 'ZoomCamera') {
+        if (_.event.params[1] == null && _.event.params[2] == null){ defaultCamZoom = _.event.params[0];
         } else {
             var tweenType = _.event.params[2];
 
@@ -15,10 +13,8 @@ function onEvent(_) {
                         defaultCamZoom = FlxG.camera.zoom;
                     }});
                 default:
-                    tweenedZoomPLEASEWORK = true;
                     zoomTween = FlxTween.tween(FlxG.camera, {zoom: _.event.params[0]}, (Conductor.stepCrochet / 1000) * _.event.params[1], {ease: Reflect.field(FlxEase, tweenType), onComplete: function(value) {
                         defaultCamZoom = FlxG.camera.zoom;
-                        tweenedZoomPLEASEWORK = false;
                     }});
             }
         }
