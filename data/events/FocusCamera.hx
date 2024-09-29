5050a69d8e07e4b6aa5f16ddcdc0fe2d32351c0c
drfiffi <drfiffi123@gmail.com>
2024-09-29 10:16:51 -0400

    Final (Optimized) Events
    holy FUCK, there!

--- data/events/FocusCamera.hx
+++ data/events/FocusCamera.hx
@@ -1,60 +1,34 @@
 import flixel.camera.FlxCameraFollowStyle;
-import funkin.backend.utils.native.HiddenProcess;
 
-var offsetCamX:Int = 0;
-var offsetCamY:Int = 0;
-var durationForTween:Int = 4;
-var easeTweenType:String;
-
-var BGTween:Bool = false;
 var LockOn:Bool = false;
 
-var chartingMode:Bool = false;
-var allowFocusVisual:Bool = false;
-
 function create(){
-    if(PlayState.chartingMode){
-        focusCameraVisual = new FlxSprite();
-        focusCameraVisual.makeGraphic(10, 10, 0xffffffff);
-        focusCameraVisual.alpha = 0;
-        focusCameraVisual.updateHitbox();
-        add(focusCameraVisual);
-        trace("\n                Welcome to Charting/DebugMode!\n      Press [SPACE] to toggle camFollow positions ON/OFF\n       (NOTE: this is only available via ChartingMenu.)");
-    }
     camFollowTween = FlxTween.tween(camFollow, {x: camFollow.x, y: camFollow.y}, 0.001);
 }
 
 function onEvent(_) {
 	switch (_.event.name){
 		case 'FocusCamera':
-            durationForTween = _.event.params[3];
-            easeTweenType = _.event.params[4];
+            var durationForTween = (Conductor.stepCrochet / 1000) * _.event.params[3];
+            var easeTweenType = _.event.params[4];
             curCameraTarget = _.event.params[0];
-            BGTween = true;
+            var camTargetX = strumLines.members[curCameraTarget].characters[0].getCameraPosition().x + (_.event.params[1] == null ? 0 : _.event.params[1]);
+            var camTargetY = strumLines.members[curCameraTarget].characters[0].getCameraPosition().y + (_.event.params[2] == null ? 0 : _.event.params[2]);
 
-            if(_.event.params[1] != null || _.event.params[1] != ""){
-                offsetCamX = _.event.params[1];
-            } else {
-                offsetCamX = 0;
-            }
-            if(_.event.params[2] != null || _.event.params[2] != ""){
-                offsetCamY = _.event.params[2];
-            } else {
-                offsetCamY = 0;
-            }
             switch(easeTweenType.toUpperCase()){
-                case "CLASSIC":
-                    LockOn = false;
-                    FlxG.camera.follow(camFollow, FlxCameraFollowStyle.LOCKON, 0.04);
-                default:
-                    LockOn = true;
-                    FlxG.camera.follow(camFollow, FlxCameraFollowStyle.LOCKON, 1);
+                case "CLASSIC": LockOn = false;
+                default: LockOn = true;
+            }
+
+            FlxG.camera.follow(camFollow, FlxCameraFollowStyle.LOCKON, (LockOn ? 1 : 0.04));
+            
+            if(camFollowTween != null) camFollowTween.cancel();
+            switch(easeTweenType.toUpperCase()){
+                case "CLASSIC" | "INSTANT": camFollow.setPosition(camTargetX, camTargetY);
+                default: camFollowTween = FlxTween.tween(camFollow, {x: camTargetX, y: camTargetY}, durationForTween, {ease: Reflect.field(FlxEase, easeTweenType)});
             }
         case "Camera Movement":
-            offsetCamX = 0;
-            offsetCamY = 0;
             if(camFollowTween != null) camFollowTween.cancel();
-            BGTween = false;
             if(LockOn){
                 LockOn = false;
                 FlxG.camera.follow(camFollow, FlxCameraFollowStyle.LOCKON, 0.04);
@@ -62,55 +36,9 @@ function onEvent(_) {
     }
 }
 
-function update(){
-    if(PlayState.chartingMode){
-        focusCameraVisual.setPosition(camFollow.x, camFollow.y);
-        if(FlxG.keys.justPressed.SPACE){
-            switch(allowFocusVisual){
-                case true: allowFocusVisual = false;
-                case false: allowFocusVisual = true;
-            }
-        }
-        switch(allowFocusVisual){
-            case false: focusCameraVisual.alpha = 0;
-            case true: focusCameraVisual.alpha = 1;
-        }
-    }
-
-    var pos = FlxPoint.get();
-	var r = 0;
-	for(c in strumLines.members[curCameraTarget].characters) {
-		var cpos = c.getCameraPosition();
-		pos.x += cpos.x;
-		pos.y += cpos.y;
-		r++;
-	}
-	if (r > 0) {
-		pos.x /= r;
-		pos.y /= r;					
-	}
-
-	if (BGTween){
-        switch(easeTweenType.toUpperCase()){
-            case "CLASSIC" | "INSTANT": camFollowOffsetsFunction(pos);
-            default: camFollowTweenFunction(pos);
-        } 
-    }
-}
-
-function camFollowTweenFunction(pos){
-    if(camFollowTween != null) camFollowTween.cancel();
-    camFollowTween = FlxTween.tween(camFollow, {x: pos.x + offsetCamX, y: pos.y + offsetCamY}, (Conductor.stepCrochet / 1000) * durationForTween, {ease: Reflect.field(FlxEase, easeTweenType)});
-}
-
-function camFollowOffsetsFunction(pos){
-    if(camFollowTween != null) camFollowTween.cancel();
-    camFollow.setPosition(pos.x + offsetCamX, pos.y + offsetCamY);
-}
-
 function onCameraMove(event){
-    switch(BGTween){
+    switch(LockOn){
         case true: event.cancelled = true;
         case false: event.cancelled = false;
-    } 
+    }
 }
--- data/events/ZoomCamera.hx
+++ data/events/ZoomCamera.hx
@@ -1,40 +1,67 @@
-function create() zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1); // just used so zoomTween.cancel() can work
+var defaultZoom:Int; 
+var lolZoom:Bool = false;
+var tweening:Bool = false;
+
+var currentCameraZoom:Float = defaultCamZoom;
+var cameraBopMultiplier:Float = 1.0;
+var defaultHUDCameraZoom:Float = FlxCamera.defaultZoom * 1.0;
+var cameraBopIntensity:Float = 0.015 * camZoomingStrength;
+var hudCameraZoomIntensity:Float = 0.015 * 2.0;
+
+function onNoteHit(event){
+    event.enableCamZooming = false;
+}
+
+function update(){
+    if(!camZooming){
+        cameraBopMultiplier = lerp(cameraBopMultiplier, 1, camGameZoomLerp); // Lerp bop multiplier back to 1.0x
+        var zoomPlusBop = defaultCamZoom * cameraBopMultiplier; // Apply camera bop multiplier.
+        FlxG.camera.zoom = zoomPlusBop;
+
+        camHUD.zoom = lerp(camHUD.zoom, defaultHudZoom, camHUDZoomLerp);
+    }
+}
+
+function beatHit(){
+    if (Options.camZoomOnBeat && !camZooming && curBeat % camZoomingInterval == 0){
+        cameraBopMultiplier += 0.015 * camZoomingStrength;
+        camHUD.zoom += 0.03 * camZoomingStrength;
+    }
+}
+
+function create(){
+    zoomTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.1); // just used so zoomTween.cancel() can work
+    defaultZoom = defaultCamZoom;
+}
 function onEvent(_) {
     if (_.event.name == 'ZoomCamera') {
         var zoomAmount = _.event.params[0];
-        var tweenTime = _.event.params[1];
+        var tweenTime = (Conductor.stepCrochet / 1000) * _.event.params[1];
         var tweenType = _.event.params[2];
-        if (tweenTime == null && tweenType == null){ defaultCamZoom = zoomAmount;
+        var zoomType = _.event.params[3];
+
+        switch(zoomType){
+            case 'stage': lolZoom = true;
+            case 'direct': lolZoom = false;
+        }
+
+        targetCameraZoom = zoomAmount * (lolZoom ? defaultZoom : 1);
+
+        if(zoomTween != null) zoomTween.cancel();
+        if (tweenTime == null && tweenType == null){
+            defaultCamZoom = zoomAmount;
         } else {
-            if(zoomTween != null) zoomTween.cancel();
-            switch(tweenType){
+            switch(tweenType.toUpperCase()){
                 case "INSTANT":
-                    zoomTween = FlxTween.tween(FlxG.camera, {zoom: zoomAmount}, 0.00000000001, {onComplete: function(value) {
-                        defaultCamZoom = FlxG.camera.zoom;
-                    }});
+                    zoomTween = FlxTween.tween(this, {defaultCamZoom: targetCameraZoom}, 0.00000000001); //using defaultCamZoom until i can rework the camera ig :sob:
                 default:
-                    zoomTween = FlxTween.tween(FlxG.camera, {zoom: zoomAmount}, (Conductor.stepCrochet / 1000) * tweenTime, {ease: Reflect.field(FlxEase, tweenType), onComplete: function(value) {
-                        defaultCamZoom = FlxG.camera.zoom;
-                    }});
+                    //zoomTween = FlxTween.tween(this, {defaultCamZoom: targetCameraZoom}, tweenTime, {ease: Reflect.field(FlxEase, tweenType)}); //using defaultCamZoom until i can rework the camera ig :sob:
+                    zoomTween = FlxTween.tween(this, {defaultCamZoom: targetCameraZoom}, tweenTime, {ease: Reflect.field(FlxEase, tweenType)});
             }
         }
     }
 }
 
-/*function update(){
-    if(tweenedZoomPLEASEWORK){
-        if(camZooming){
-            camZoomingStrength = lerp(1.0, camZoomingStrength, 0.05); // Lerp bop multiplier back to 1.0x
-            var zoomPlusBop = FlxG.camera.zoom * camZoomingStrength; // Apply camera bop multiplier.
-            FlxG.camera.zoom = zoomPlusBop;
-        }
-    }
-}
-    
-function beatHit(curBeat){
-    if(Options.camZoomOnBeat && camZooming && FlxG.camera.zoom < maxCamZoom && curBeat % camZoomingInterval == 0){
-        if(tweenedZoomPLEASEWORK){
-            FlxG.camera.zoom += 0.015 * camZoomingStrength;
-        }
-    }
-}*/
+/*  For context, im not too sure that base game handles currentCameraZoom the same as defaultCamZoom because even though they are 
+    tied to FlxG.camera.zoom, FlxCamera.defaultZoom is used with currentCameraZoom where as defaultCamZoom is playing the roles
+    of both currentCameraZoom AND FlxCamera.defaultZoom,,,*/
